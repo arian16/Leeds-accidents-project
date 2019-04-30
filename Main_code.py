31bf040b3a753293d3943e0ced1d94af4adc984a
arian16 <49404774+arian16@users.noreply.github.com>
2019-04-30 09:40:35 -0400

    Update Main_code.py

--- Main_code.py
+++ Main_code.py
@@ -45,5 +45,39 @@ ax.legend(['Cases','Casualties','Vehicles'],loc=4)
 ax.spines['right'].set_visible(False) # plt version?
 ax.spines['top'].set_visible(False)
 ax.set_title('Number of cases/casualties/vehicles from 2005-2017') # plt.title
-plt.savefig('/Users/arian/Documents/Cases_casualties_vehicles_2005_to_2017.png')
+plt.savefig('/Users/arian/Documents/Pictures/Cases_casualties_vehicles_2005_to_2017.png')
+plt.show()
+######## Cases per day of week and month #######
+
+# Creating a separate column for year, month and day
+df1['year']=df1['date'].str.slice(start=6).astype(int) # Extract year from str and changed to int
+df1['month']=df1['date'].str.slice(start=3,stop=-5).astype(int) # Extract month from str and changed to int
+df1['day']=df1['date'].str.slice(start=0,stop=-8).astype(int) # Extract day from str and changed to int 
+#Day of week
+cases_per_dow=value_count_calculator(df1['day_of_week'],'cases','dow')
+days_of_week_map={1:'Sunday', 2:'Monday', 3:'Tuesday', 4:'Wednesday', 
+                  5:'Thursday', 6:'Friday', 7:'Saturday'}
+cases_per_dow['dow_new']=cases_per_dow['dow'].map(days_of_week_map)
+#Month
+cases_per_month=value_count_calculator(df1['month'],'cases','month')
+month_map={1:'January', 2:'February', 3:'March', 4:'April', 
+                  5:'May', 6:'June', 7:'July', 8:'August',9:'September',
+                  10:'October', 11:'November', 12:'December'}
+cases_per_month['month_new']=cases_per_month['month'].map(month_map)
+# Plot 2
+fig, (ax1, ax2) = plt.subplots(2,1,figsize=(10,10))
+ax1.bar(cases_per_dow['dow_new'],cases_per_dow['cases'])
+ax1.set_ylim(bottom=0, top=1100)
+ax1.set_ylabel('Cases')
+ax1.spines['right'].set_visible(False) # plt version?
+ax1.spines['top'].set_visible(False)
+ax1.set_title('Number of cases per day of week from 2005-2017',fontsize=14)
+ax2.bar(cases_per_month['month_new'],cases_per_month['cases'])
+ax2.set_ylim(bottom=0, top=700)
+ax2.set_ylabel('Cases')
+ax2.spines['right'].set_visible(False) # plt version?
+ax2.spines['top'].set_visible(False)
+ax2.set_title('Number of cases per month from 2005-2017', fontsize=14)
+plt.xticks(rotation=90) # Doesn't work with ax
+plt.savefig('/Users/Documents/Data science project/Pictures/Cases_wrt_month&dow.png')
 plt.show()
